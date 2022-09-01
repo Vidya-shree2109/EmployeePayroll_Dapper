498930b9c96306b2f311f02ac1b6d85e4d656af5
Vidya <vidyashree2109@gmail.com>
2022-09-01 12:03:10 +0530

    [Vidya] Added : Welcome To Employee Payroll Dapper Project

---
+++ Dapper_EmployeePayroll/Dapper_EmployeePayroll/Controllers/EmployeeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper_EmployeePayroll.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+    }
+}
